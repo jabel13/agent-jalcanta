4b89aa0fb4717902e4f7945bb449541f0878ec59
jabel13 <jalcanta@oswego.edu>
2023-10-10 23:39:26 -0400

    Update main.go

--- main.go
+++ main.go
@@ -30,6 +30,53 @@ type Game struct {
     Bookmakers    []Bookmaker `json:"bookmakers"`
 }
 
+func getAPIResponse(apiUrl string) (*http.Response, error) {
+    response, err := http.Get(apiUrl)
+    if err != nil {
+        return nil, err
+    }
+
+    if response.StatusCode != http.StatusOK {
+        return nil, fmt.Errorf("API returned a non-OK status code: %d", response.StatusCode)
+    }
+    return response, nil
+}
+
+func readAndParseResponse(response *http.Response) ([]Game, int, error) {
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        return nil, 0, err
+    }
+    
+	contentSize := len(body)
+
+    var games []Game
+    err = json.Unmarshal(body, &games)
+
+    if err != nil {
+        return nil, 0, err
+    }
+    return games, contentSize, nil
+}
+
+func printGameDetails(games []Game) {
+    for _, game := range games {
+        fmt.Println("Game ID:", game.ID)
+        for _, bookmaker := range game.Bookmakers {
+            fmt.Println()
+            fmt.Println("Bookmaker Title:", bookmaker.Title)
+            for _, market := range bookmaker.Markets {
+                fmt.Println("Outcomes:")
+                for _, outcome := range market.Outcomes {
+                    fmt.Printf("  Name: %s, Price: %d\n", outcome.Name, outcome.Price)
+                }
+            }
+        }
+        fmt.Println() // Add an empty line between each game
+    }
+}
+
+
 func proccessMLBOdds() {
 
 	var tag string
@@ -41,55 +88,22 @@ func proccessMLBOdds() {
 	apiUrl := "https://api.the-odds-api.com/v4/sports/baseball_mlb/odds/?apiKey=" + apiKey + "&regions=us" + "&markets=h2h" + "&oddsFormat=american"
 
     // Make the HTTP GET request with the updated URL
-    response, err := http.Get(apiUrl)
+    response, err := getAPIResponse(apiUrl)
     if err != nil {
         fmt.Println("Error:", err)
         return
     }
     defer response.Body.Close()
 
-	// Check if the response status code is not 200 OK
-	if response.StatusCode != http.StatusOK {
-		fmt.Printf("API returned a non-OK status code: %d\n", response.StatusCode)
-		return
-	}
 
-	// Read the response body into a byte slice
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
-	}
+	games, contentSize, err := readAndParseResponse(response)
 
-	// Calculate the size of the JSON content
-	contentSize := len(body)
-	
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
 
-	// Decode the JSON response into a slice of Bookmaker structs
-	var games []Game
-	err = json.Unmarshal(body, &games)
-	if err != nil {
-		fmt.Println("Error decoding JSON:", err)
-		return
-	}
-
-	for _, game := range games {
-		fmt.Println("Game ID:", game.ID)
-		// Loop through the games and print the bookmaker title, markets key, and outcomes
-		for _, bookmaker := range game.Bookmakers {
-			fmt.Println()
-			fmt.Println("Bookmaker Title:", bookmaker.Title)
-
-			for _, market := range bookmaker.Markets {
-
-				fmt.Println("Outcomes:")
-				for _, outcome := range market.Outcomes {
-					fmt.Printf("  Name: %s, Price: %d\n", outcome.Name, outcome.Price)
-				}
-			}
-		}
-		fmt.Println() // Add an empty line between each game
-	}
+	printGameDetails(games)
 
 	// Valid EchoSend (message echoed to console and no error returned)
 	err = client.EchoSend("debug", "This is a debug message")
@@ -101,6 +115,7 @@ func proccessMLBOdds() {
 
 	fmt.Printf("Size of JSON content: %d bytes\n", contentSize)
 
+
 }
 
 func main() {
@@ -109,7 +124,10 @@ func main() {
 		proccessMLBOdds()
 
 		// Sleep for 120 minutes before the next request
-		time.Sleep(120 * time.Minute)
+		// time.Sleep(120 * time.Minute)
+
+		// Testing purposes
+		time.Sleep(2 * time.Minute)
 	}
 
 }
