247518bc4ad20fb79ae72a39bb3ba6e717dbc002
jabel13 <jalcanta@oswego.edu>
2023-10-19 16:34:31 -0400

    importing the AWS SDK

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+    "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"encoding/json"
 	"fmt"
 	"net/http"
